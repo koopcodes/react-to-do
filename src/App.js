28d4ab82cae3e3314c6e1f2b27ad8ece6b93775d
koopdev <koop@koop.codes>
2018-11-06 10:09:45 -0800

    Update

--- src/App.js
+++ src/App.js
@@ -3,65 +3,77 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            todos: [
-                { description: "Walk the cat", isCompleted: true },
-                { description: "Throw the dishes away", isCompleted: false },
-                { description: "Buy new dishes", isCompleted: false }
-            ],
-            newTodoDescription: ''
-        };
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			todos: [
+				{ description: 'Walk the cat', isCompleted: true },
+				{ description: 'Throw the dishes away', isCompleted: false },
+				{ description: 'Buy new dishes', isCompleted: false },
+			],
+			newTodoDescription: '',
+		};
+	}
 
-    deleteTodo(index) {
-        const list = this.state.todos.slice();
-        const newList = list.filter((todo, i) => i !== index);
-        this.setState({ todos: newList });
-    }
+	deleteTodo(index) {
+		const list = this.state.todos.slice();
+		const newList = list.filter((todo, i) => i !== index);
+		this.setState({ todos: newList });
+	}
 
-    handleChange(e) {
-        this.setState({ newTodoDescription: e.target.value })
-    }
+	handleChange(e) {
+		this.setState({ newTodoDescription: e.target.value });
+	}
 
-    handleSubmit(e) {
-        e.preventDefault();
-        if (!this.state.newTodoDescription) { return }
-        const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
-        this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' });
-    }
+	handleSubmit(e) {
+		e.preventDefault();
+		if (!this.state.newTodoDescription) {
+			return;
+		}
+		const newTodo = {
+			description: this.state.newTodoDescription,
+			isCompleted: false,
+		};
+		this.setState({
+			todos: [...this.state.todos, newTodo],
+			newTodoDescription: '',
+		});
+	}
 
-    toggleComplete(index) {
-        const todos = this.state.todos.slice();
-        const todo = todos[index];
-        todo.isCompleted = todo.isCompleted ? false : true;
-        this.setState({ todos: todos });
-    }
+	toggleComplete(index) {
+		const todos = this.state.todos.slice();
+		const todo = todos[index];
+		todo.isCompleted = todo.isCompleted ? false : true;
+		this.setState({ todos: todos });
+	}
 
-    render() {
-        return (
-            <div className="App">
-                <h1>To Do List</h1>
-                <ol>
-                    {this.state.todos.map((todo, index) =>
-                        <ToDo
-                            key={index}
-                            description={todo.description}
-                            isCompleted={todo.isCompleted}
-                            toggleComplete={() => this.toggleComplete(index)}
-                            deleteTodo={() => this.deleteTodo(index)}
-                        />
-                    )}
-                </ol>
-                <form onSubmit={(e) => this.handleSubmit(e)} >
-                    <input type="text" placeholder="Add your To-Do's here" value={this.state.newTodoDescription} onChange={(e) => this.handleChange(e)} />
-                    <input type="submit" value="Add" />
-                </form>
-
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="App">
+				<h1>To Do List</h1>
+				<ol>
+					{this.state.todos.map((todo, index) => (
+						<ToDo
+							key={index}
+							description={todo.description}
+							isCompleted={todo.isCompleted}
+							toggleComplete={() => this.toggleComplete(index)}
+							deleteTodo={() => this.deleteTodo(index)}
+						/>
+					))}
+				</ol>
+				<form onSubmit={(e) => this.handleSubmit(e)}>
+					<input
+						type="text"
+						placeholder="Add your To-Do's here"
+						value={this.state.newTodoDescription}
+						onChange={(e) => this.handleChange(e)}
+					/>
+					<input type="submit" value="Add" />
+				</form>
+			</div>
+		);
+	}
 }
 
 export default App;
